e87fe8c51d6d82d8d12b5e645c3d276a2074f661
CeSium <fedir.katushonok@gmail.com>
2024-10-05 21:40:12 +0300

    Refactor wheelset.py to improve wheel rendering

--- cart/wheelset.py
+++ cart/wheelset.py
@@ -6,19 +6,34 @@ AXEL_HORIZONTAL_OFFSET = 30
 AXEL_RADIUS = 5
 WHEEL_INNER_RADIUS = AXEL_RADIUS + 0.1
 AXEL_LENGTH = 60
+WHEEL_MIN_WIDTH = 4
+WHEEL_MAX_WIDTH = 8
+WHEEL_OUTER_RADIOS = 10
 
 res = union()
 
 for i in [-AXEL_HORIZONTAL_OFFSET, AXEL_HORIZONTAL_OFFSET]:
     direction_wheelset = union()
     for j in [-1, 1]:
-        direction_wheelset += translate([0, j * ((AXEL_LENGTH / 2) - 4), 0])(
-            rotate([90, 0, 0])(
-                rotate_extrude(360)(
-                    translate([WHEEL_INNER_RADIUS, 0])(
-                        square([1, 4], center=True)
-                    )
+
+        tire = rotate([90, 0, 0])(
+            rotate_extrude(360)(
+                polygon(points=[[-WHEEL_INNER_RADIUS, -WHEEL_MIN_WIDTH/2], [-WHEEL_INNER_RADIUS, WHEEL_MIN_WIDTH/2], [-WHEEL_OUTER_RADIOS, WHEEL_MAX_WIDTH/2], [-WHEEL_OUTER_RADIOS, -WHEEL_MAX_WIDTH/2]])
+            )
+        )
+
+        rim = rotate([90, 0, 0])(
+            rotate_extrude(360)(
+                translate([WHEEL_INNER_RADIUS, 0])(
+                    square([1, WHEEL_MIN_WIDTH], center=True)
                 )
+            )
+        )
+
+        direction_wheelset += translate([0, j * ((AXEL_LENGTH / 2) - 4), 0])(
+            union()(
+                tire,
+                rim
             )
         )
 
