3323cc69e0832b86704541bef9655a639b40499d
CeSium <fedir.katushonok@gmail.com>
2024-11-02 16:47:12 +0200

    Added rays for stability

--- gimbal_base/gimbal_base.py
+++ gimbal_base/gimbal_base.py
@@ -7,8 +7,8 @@ OUTPUT_FILE_NAME = 'gimbal_base.scad'
 BASE_THICKNESS = 4
 BASE_WIDTH = 60
 ATTACHMENT_WIDTH = 39.5
-RAY_LENGTH = 90
-RAY_WIDTH = 30
+RAY_LENGTH = 150
+RAY_WIDTH = 15
 RAY_THICKNESS = BASE_THICKNESS
 
 res = union()
@@ -25,13 +25,11 @@ for i in [(-ATTACHMENT_WIDTH / 2) + 4.3, (ATTACHMENT_WIDTH / 2) - 4.3]:
     for j in [(-ATTACHMENT_WIDTH / 2) + 4.3, (ATTACHMENT_WIDTH / 2) - 4.3]:
         mounting_holes += translate([i, j, 0])(mounting_hole)
 
-for i in [(-BASE_WIDTH / 2) - (RAY_LENGTH / 2), (BASE_WIDTH / 2) + (RAY_LENGTH / 2)]:
-        rays += translate([i, 0, 0])(cube([RAY_LENGTH, RAY_WIDTH, RAY_THICKNESS], center=True))
+for i in range(0, 180, 45):
+    rays += rotate([0, 0, i])(cube([RAY_LENGTH, RAY_WIDTH, RAY_THICKNESS], center=True))
 
-for i in [(-BASE_WIDTH / 2) - (RAY_LENGTH / 2), (BASE_WIDTH / 2) + (RAY_LENGTH / 2)]:
-        rays += translate([0, i, 0])(cube([RAY_WIDTH, RAY_LENGTH, RAY_THICKNESS], center=True))
-
-res += square_base - mounting_holes
 res += rays
+res += square_base
+res -= mounting_holes
 
 scad_render_to_file(res, OUTPUT_FILE_NAME, file_header='$fa = 1;\n$fs = 0.1;')
